edf7127122d8559ad60c5662799962695c5f5f97
EvgeniyDanisevich <a937va@gmail.com>
2021-06-18 17:08:45 +0300

    Scheme fixed

--- db/scheme.sql
+++ db/scheme.sql
@@ -6,19 +6,19 @@ create table if not exists cars (
     price double precision
 );
 
-create table photo (
+create table if not exists photo (
     id serial primary key,
     path varchar(255)
 );
 
-create table users(
+create table if not exists users(
     id serial primary key,
     name varchar(255),
     email varchar(255) not null unique,
     password varchar(255)
 );
 
-create table ads (
+create table if not exists ads (
     id serial primary key,
     description text,
     car_id int references cars(id),
@@ -27,4 +27,3 @@ create table ads (
     user_id int references users(id),
     created timestamp
 );
-
